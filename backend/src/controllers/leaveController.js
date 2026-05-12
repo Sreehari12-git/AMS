5a30f1c22af510661402aa80ebe9163c9be326fa
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-05-12 14:26:06 +0530

    Get leaves

--- backend/src/controllers/leaveController.js
+++ backend/src/controllers/leaveController.js
@@ -21,3 +21,28 @@ export const applyLeave = async(req,res) => {
         res.status(500).json({message: "Server  error"});
     }
 }
+
+export const getMyLeaves = async(req,res) => {
+    try {
+        const userId = req.user.id;
+        
+        const leaves = await prisma.leave.findMany({
+            where: {
+                userId
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+        
+        res.status(200).json(leaves);
+    } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server error"
+    });
+
+  }
+};
